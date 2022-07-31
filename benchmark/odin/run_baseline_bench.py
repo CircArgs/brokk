81db5523142905f78c4358f45b76106ccf79e8cd
nick ouellet <quebecname@gmail.com>
2022-07-30 21:23:47 -0400

    baseline timings

---
+++ benchmark/odin/run_baseline_bench.py
@@ -0,0 +1,20 @@
+import os
+
+import pandas as pd
+from tqdm import tqdm
+
+mappings={'s':1/1000000, 'ms':1/1000, 'µs': 1, 'ns': 1000}
+
+timings=[]
+
+for _ in tqdm(range(100)):
+    out=os.popen('./baseline.bin').read()
+    out=out.split(' ')
+    timings.append(dict(zip(('left_size', 'shared_size', 'right_size', 'time'), out)))
+
+df=pd.DataFrame(timings)
+df.time=df.time.str.strip()
+df['magnitude']=df.time.str.replace(r'[0-9]*\.?[0-9]*', '', regex=True)
+df.time=df.time.str.replace(r'[^\d\.]', '', regex=True).astype(float)
+df.time/=df.magnitude.map(mappings)
+df.to_csv('baseline_timings.csv', index=False)
