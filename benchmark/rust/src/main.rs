9d2710ec98d26477badf1112fc91d2f7d5569fb6
nick ouellet <quebecname@gmail.com>
2022-07-30 21:04:02 -0400

    rust benchmark run

--- benchmark/rust/src/main.rs
+++ benchmark/rust/src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     let shared_size = rng.gen_range(0..1000) + 1;
     let right_size = rng.gen_range(0..1000) + 1;
     let left: DMatrix<f32> =
-        DMatrix::from_fn(left_size, shared_size, |_, _| rng.gen_range(0.0, 100.0));
+        DMatrix::from_fn(left_size, shared_size, |_, _| rng.gen_range(0.0..100.0));
     let right: DMatrix<f32> =
-        DMatrix::from_fn(shared_size, right_size, |_, _| rng.gen_range(0.0, 100.0));
+        DMatrix::from_fn(shared_size, right_size, |_, _| rng.gen_range(0.0..100.0));
 
     let start = Instant::now();
-    let m = left * right;
+    let _ = left * right;
     let end = start.elapsed();
     println!("{} {} {} {:?}", left_size, shared_size, right_size, end);
 }
